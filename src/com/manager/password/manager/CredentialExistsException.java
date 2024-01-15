30a7193363c48f66f0de5b415a636498e410d7f8
GrewalCreator <gurtejgrewal455@gmail.com>
2024-01-15 12:06:34 -0500

    Basic Chrome Extension Setup

--- src/com/manager/password/manager/CredentialExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.manager.password.manager;
-
-public class CredentialExistsException extends RuntimeException{
-    public CredentialExistsException(String errorMessage){
-        super(errorMessage);
-    }
-}
